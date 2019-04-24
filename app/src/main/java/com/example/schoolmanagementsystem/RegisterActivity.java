7ca616c29dd5a62848cd44aac0e6423a738fa8d0
DESKTOP-58EJ12O\Hafiz Naeem Hassan <naeem.alpha2labs1@gmail.com>
2019-04-24 06:23:15 +0500

    add ids in activity class

--- app/src/main/java/com/example/schoolmanagementsystem/RegisterActivity.java
+++ app/src/main/java/com/example/schoolmanagementsystem/RegisterActivity.java
@@ -2,12 +2,20 @@ package com.example.schoolmanagementsystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText editTextname, editTextemail, editTextpassword;
+    private Button btnregister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        editTextname = findViewById(R.id.editname);
+        editTextemail = findViewById(R.id.editemail);
+        editTextpassword = findViewById(R.id.editpswrd);
+        btnregister = findViewById(R.id.btnlogin);
     }
 }
