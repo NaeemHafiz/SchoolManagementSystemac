19028424a4beafc05149ee80e9887736d9f3782a
DESKTOP-58EJ12O\Hafiz Naeem Hassan <naeem.alpha2labs1@gmail.com>
2019-04-24 05:57:49 +0500

    add table name,db name

--- app/src/main/java/com/example/schoolmanagementsystem/DatabaseHelper.java
+++ app/src/main/java/com/example/schoolmanagementsystem/DatabaseHelper.java
@@ -1,17 +1,55 @@
 package com.example.schoolmanagementsystem;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "user.db";
+    public static final String TABLE_NAME = "usertable";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "EMAIL";
+    public static final String COL_4 = "PASSWORD";
 
-    @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,EMAIL TEXT,PASSWORD TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean saveDataSqLite(String name, String email, String password) {
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, email);
+        contentValues.put(COL_4, password);
+        long success = database.insert(TABLE_NAME, null, contentValues);
+        if (success == -1)
+            return false;
+        else return true;
+    }
+
+    public Cursor getAllData() {
+        int id = 16;
+        String name = "helo";
+        SQLiteDatabase database = this.getReadableDatabase();
+        String query = "select * from " + TABLE_NAME + " where ID =? AND NAME=?  LIMIT 1 ";
+        String[] selectionArgs = {String.valueOf(id), name};
+        Cursor cursor = database.rawQuery(query, selectionArgs);
+        return cursor;
 
     }
 }
